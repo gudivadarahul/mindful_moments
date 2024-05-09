69b2b5cee1f60fa4c05ba30afa463236253da634
gudivadarahul <gudivadarahul@gmail.com>
2024-05-09 18:49:42 -0500

    added Home Page component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import Navbar from './components/Navbar';
 import UserPage from './components/UserPage';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -11,10 +12,12 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/user-page" element={<UserPage />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />  // Set Home as the default route
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<div>Mindful Moments Landing Page</div>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user-page" element={<UserPage />} />
+          {/* Redirect all other paths to Home, can also be set to a 404 page */}
+          <Route path="*" element={<Home />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Our Application!</h1>
+      <p>This is your starting point to a better you. Track your goals, progress, and achievements with ease.</p>
+    </div>
+  );
+}
+
+export default Home;
