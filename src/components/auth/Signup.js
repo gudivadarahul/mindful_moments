c934625e095e64d4e0fc729d3efdf8ddee02e16b
gudivadarahul <gudivadarahul@gmail.com>
2024-05-11 14:03:21 -0500

    added confirm password func

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Signup.css";  // Assuming you're using the same CSS file for simplicity
+import "./Signup.css";
 
 function Signup() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch('/users/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password })
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Passwords do not match.");
+      return;
+    }
+    if (password.length < 5) {
+      setError("Password must be at least 5 characters long.");
+      return;
+    }
+
+    const response = await fetch("/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
     });
+
     if (response.ok) {
-      navigate('/user-page');
+      navigate("/user-page");
     } else {
       const errorText = await response.text();
       setError(errorText);
@@ -26,7 +38,9 @@ function Signup() {
   return (
     <div className="signup-page">
       <nav className="home-nav">
-        <Link to="/" className="brand-name">Mindful Moments</Link>
+        <Link to="/" className="brand-name">
+          Mindful Moments
+        </Link>
       </nav>
       <div className="signup-container">
         <form onSubmit={handleSubmit} className="signup-form">
@@ -45,10 +59,17 @@ function Signup() {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
           <button type="submit">Sign Up</button>
           <button
             type="button"
-            onClick={() => navigate('/login')}
+            onClick={() => navigate("/login")}
             className="button"
           >
             Log in
