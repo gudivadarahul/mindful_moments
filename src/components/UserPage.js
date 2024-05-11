9b1227d171cf313fcf24d492c4fba91e994a4f0f
gudivadarahul <gudivadarahul@gmail.com>
2024-05-11 16:18:55 -0500

    added styling for UserPage for weekly goals and added default goals for new users

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -14,7 +14,9 @@ function UserPage() {
   useEffect(() => {
     const checkSession = async () => {
       try {
-        const response = await fetch("/users/check-session", { credentials: "include" });
+        const response = await fetch("/users/check-session", {
+          credentials: "include",
+        });
         const data = await response.json();
         if (!data.isLoggedIn) {
           navigate("/login");
@@ -43,19 +45,30 @@ function UserPage() {
 
   const fetchWeeklyGoals = async () => {
     try {
-      const response = await fetch(`/api/weekly-goals`, { credentials: "include" });
+      const response = await fetch(`/api/weekly-goals`, {
+        credentials: "include",
+      });
       if (!response.ok) throw new Error("Failed to fetch goals");
       const data = await response.json();
-      const goalsWithIds = data.goals ? data.goals.map(goal => ({ ...goal, id: nanoid() })) : [];
-      setWeeklyGoals(goalsWithIds);
+      if (data.goals && data.goals.length > 0) {
+        const goalsWithIds = data.goals.map((goal) => ({ ...goal, id: nanoid() }));
+        setWeeklyGoals(goalsWithIds);
+      } else {
+        const defaultGoals = [
+          { id: nanoid(), text: "", completed: false },
+          { id: nanoid(), text: "", completed: false },
+          { id: nanoid(), text: "", completed: false },
+        ];
+        setWeeklyGoals(defaultGoals);
+      }
     } catch (error) {
       console.error("Error fetching weekly goals:", error);
     }
   };
 
   const updateGoalText = (id, text) => {
-    setWeeklyGoals(prevGoals =>
-      prevGoals.map(goal => (goal.id === id ? { ...goal, text: text } : goal))
+    setWeeklyGoals((prevGoals) =>
+      prevGoals.map((goal) => (goal.id === id ? { ...goal, text: text } : goal))
     );
     setUnsavedChanges(true);
   };
@@ -70,19 +83,16 @@ function UserPage() {
   };
 
   const handleLogout = async () => {
-    if (unsavedChanges) {
-      try {
-        const response = await fetch("/api/weekly-goals", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ goals: weeklyGoals }),
-          credentials: "include",
-        });
-        if (!response.ok) throw new Error("Failed to save goals");
-      } catch (error) {
-        console.error("Error saving goals:", error);
-        // You can also show an error message to the user
-      }
+    try {
+      const response = await fetch("/api/weekly-goals", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ goals: weeklyGoals }),
+        credentials: "include",
+      });
+      if (!response.ok) throw new Error("Failed to save goals");
+    } catch (error) {
+      console.error("Error saving goals:", error);
     }
     try {
       const response = await fetch("/users/logout");
@@ -111,45 +121,87 @@ function UserPage() {
 
   return (
     <div className="user-page-container">
-      <h1>Welcome {username}</h1>
-      <div className="goals-container">
-        <h2>Your Weekly Goals</h2>
-        {weeklyGoals.length > 0 ? (
-          <ol>
-            {weeklyGoals.map((goal) => (
-              <li key={goal.id} className={`goal-item ${goal.completed ? "completed" : ""}`}>
-                <input
-                  type="checkbox"
-                  checked={goal.completed}
-                  onChange={() => toggleGoalCompletion(goal.id)}
-                  id={`goal-${goal.id}-checkbox`}
-                />
-                <input
-                  type="text"
-                  value={goal.text}
-                  onChange={(e) => updateGoalText(goal.id, e.target.value)}
-                  className="goal-input"
-                  id={`goal-${goal.id}`}
-                  name="goal-text"
-                />
-              </li>
-            ))}
-          </ol>
-        ) : (
-          <p>No goals to display.</p>
+      <nav className="user-nav">
+        <a href="/" className="brand-name">
+          Mindful Moments
+        </a>
+        <div className="nav-links">
+          <button onClick={handleLogout} className="logout-button">
+            Log Out
+          </button>
+          <button
+            onClick={() => setShowPasswordForm(!showPasswordForm)}
+            className="change-password-button"
+          >
+            <span className="button-text">Change Password</span>
+          </button>
+        </div>
+      </nav>
+      <div className="user-info">
+        <span className="welcome-message">Welcome, {username}</span>
+      </div>
+      <div className="main-content">
+        <div className="goals-container">
+          <h2>Your Weekly Goals</h2>
+          {weeklyGoals.length > 0 ? (
+            <ol>
+              {weeklyGoals.map((goal) => (
+                <li
+                  key={goal.id}
+                  className={`goal-item ${goal.completed ? "completed" : ""}`}
+                >
+                  <input
+                    type="checkbox"
+                    checked={goal.completed}
+                    onChange={() => toggleGoalCompletion(goal.id)}
+                    className="goal-checkbox"
+                  />
+                  <input
+                    type="text"
+                    value={goal.text}
+                    onChange={(e) => updateGoalText(goal.id, e.target.value)}
+                    className="goal-input"
+                    name="goal-text"
+                  />
+                </li>
+              ))}
+            </ol>
+          ) : (
+            <ol>
+              {[1, 2, 3].map((index) => (
+                <li key={index} className="goal-item">
+                  <input type="checkbox" className="goal-checkbox" />
+                  <input
+                    type="text"
+                    onChange={(e) => updateGoalText(index, e.target.value)}
+                    className="goal-input"
+                    name="goal-text"
+                  />
+                </li>
+              ))}
+            </ol>
+          )}
+        </div>
+        {showPasswordForm && (
+          <form onSubmit={handleUpdatePassword} className="form-container">
+            <label className="form-label">
+              New Password:
+              <input
+                type="password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                className="input-field"
+              />
+            </label>
+            <button type="submit" className="submit-button">
+              Update Password
+            </button>
+          </form>
         )}
       </div>
-      <button onClick={handleLogout} className="logout-button">Log Out</button>
-      <button onClick={() => setShowPasswordForm(!showPasswordForm)} className="change-password-button">Change Password</button>
-      {showPasswordForm && (
-        <form onSubmit={handleUpdatePassword} className="form-container">
-          <label className="form-label">
-            New Password:
-            <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} className="input-field" />
-          </label>
-          <button type="submit" className="submit-button">Update Password</button>
-        </form>
-      )}
+      <footer className="user-footer">
+        <p>© 2024 Rahul Gudivada. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
